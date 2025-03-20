cb631a3e147896d48787bced768f81da2f073e3a
Aditya Sahu <adityasahu1511@gmail.com>
2025-03-20 16:30:21 -0400

    finish project

---
+++ src/components/Cart.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+/*
+Make your own Cart component, inside the components folder to show the number of the items in the Cart
+*/
+export default function Cart ({info}){
+    console.log("cart.jsx: ",info);
+    return(
+        <div>
+            <h1>Cart</h1>
+            <p>Number of items in the cart: {info}</p>
+        </div>
+    )
+}
